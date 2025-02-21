0e52ba8e5595e18b32fc9913ac40c4b200299c4c
Furqan Riansyah <putra.furqan12@gmail.com>
2025-02-22 02:17:21 +0700

    update: add led blink

--- main/main.cpp
+++ main/main.cpp
@@ -9,9 +9,30 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
-void app_main(void)
+#include "driver/gpio.h"
+
+#define BUILTIN_LED_GPIO ((gpio_num_t)48)
+
+bool state = false;
+
+extern "C" void hardware_init(){
+    gpio_reset_pin(BUILTIN_LED_GPIO);
+    gpio_set_direction(BUILTIN_LED_GPIO, GPIO_MODE_OUTPUT);
+}
+
+extern "C" void app_main(void)
 {
-    printf("Hello world!\n");
+    hardware_init();
+
+    while (1)
+    {
+        ESP_LOGI("MAIN", "Turning the LED %s!", state == true ? "ON" : "OFF");
+        state = !state;
+        gpio_set_level(BUILTIN_LED_GPIO, state);
+        vTaskDelay(500);
+    }
+    
 
 }
