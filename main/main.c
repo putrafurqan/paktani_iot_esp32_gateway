fdc5a09aadc5a110278f3a8db37b24d1d330b148
Furqan Riansyah <putra.furqan12@gmail.com>
2025-02-22 00:52:05 +0700

    initial commit

---
+++ main/main.c
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+}
